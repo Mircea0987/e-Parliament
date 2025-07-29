2c02acfa30b5e28da5885d0ad338849880a6ff04
mircostin <fedemir04@gmail.com>
2025-07-29 15:44:54 +0300

    playing with EF Core MVC structure

---
+++ e-Parliament/Services/Users.cs
@@ -0,0 +1,47 @@
+﻿using e_Parliament.DbContextApp;
+using e_Parliament.models; // Add this using directive to reference the correct Users model
+using e_Parliament.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Parliament.Services
+{
+    internal class UsersService : UserRepository
+    {
+        private AppDbContext _context;
+        public List<Users> getAllUsers()
+        {
+            using (_context = new AppDbContext())
+            {
+                return _context.users.ToList();
+            }
+        }
+
+        public Users getUserById(int id)
+        {
+            using (_context = new AppDbContext())
+            {
+                Users user = _context.users.FirstOrDefault(u => u.Id == id);
+                if(user == null)
+                {
+                    throw new Exception("User not found");
+                }
+                else
+                {
+                  return user;
+                }
+            }
+        }
+        public void addUser(Users user)
+        {
+            using(_context = new AppDbContext())
+            {
+                _context.users.Add(user);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
--- e-Parliament/models/Users.cs
+++ e-Parliament/models/Users.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,13 @@ namespace e_Parliament.models
 {
     internal class Users
     {
+        [Key]
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? HashPassword { get; set; }
+        [Column("firstName")]
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } ="";
+        public string HashPassword { get; set; } = "";
         public DateOnly DateOfBirth { get; set; }
         public string Country { get; set; }
         public string? PhoneNumber { get; set; }
